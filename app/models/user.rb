980db1aeb94e32abde2ad5d9a00e252f3da08ad7
J Silverstein <jess.pebble@gmail.com>
2020-09-23 17:48:31 -0400

    on the route to working login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   enum user_type: [ :student, :instructor ]
   validates :user_type, inclusion: { in: user_types.keys }
+  validates :username, presence: true, uniqueness: true
+
 end
