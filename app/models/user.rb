06dbdac6bf87b13994a84a13f516e0c1f6a043a1
J Silverstein <jess.pebble@gmail.com>
2020-09-23 19:30:15 -0400

    fix enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  enum user_type: [ :student, :instructor ]
-  validates :user_type, inclusion: { in: user_types.keys }
+  enum user_type: %w(student instructor)
   validates :username, presence: true, uniqueness: true
 
+  scope :instructors, -> { where(user_type: 'instructor') }
+  scope :students, -> { where(user_type: 'student') }
 end
